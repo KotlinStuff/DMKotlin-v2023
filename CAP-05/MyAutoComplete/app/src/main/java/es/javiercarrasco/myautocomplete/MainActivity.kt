73772fb505eeba35661b5e0c33893747bc25b403
Javier Carrasco <carrasco.javi@gmail.com>
2023-02-15 19:11:12 +0100

    Capítulo 5
    Elementos complejos en Android

--- CAP-05/MyAutoComplete/app/src/main/java/es/javiercarrasco/myautocomplete/MainActivity.kt
+++ CAP-05/MyAutoComplete/app/src/main/java/es/javiercarrasco/myautocomplete/MainActivity.kt
@@ -1,6 +1,10 @@
 package es.javiercarrasco.myautocomplete
 
 import android.os.Bundle
+import android.widget.Adapter
+import android.widget.ArrayAdapter
+import android.widget.SimpleAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import es.javiercarrasco.myautocomplete.databinding.ActivityMainBinding
 
@@ -12,6 +16,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setUpAutoComplete()
+    }
 
+    private fun setUpAutoComplete() {
+        // Se crea un adapter de tipo Array.
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.array_nombres,
+            android.R.layout.simple_list_item_1
+        )
+        // Se asigna el adapter al AutoComplete.
+        binding.autoCompleteTextView.setAdapter(adapter)
+
+        binding.autoCompleteTextView.setOnItemClickListener { adapterView, view, i, l ->
+            Toast.makeText(this, binding.autoCompleteTextView.text, Toast.LENGTH_SHORT).show()
+        }
     }
 }
