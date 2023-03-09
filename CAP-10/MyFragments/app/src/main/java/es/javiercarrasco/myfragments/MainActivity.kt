d91d628b2190a5bbdeec515daa60c7a6bd8cbdf1
Javier Carrasco <carrasco.javi@gmail.com>
2023-03-09 13:12:34 +0100

    Capítulo 10
    Fragments

---
+++ CAP-10/MyFragments/app/src/main/java/es/javiercarrasco/myfragments/MainActivity.kt
@@ -0,0 +1,16 @@
+package es.javiercarrasco.myfragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import es.javiercarrasco.myfragments.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
