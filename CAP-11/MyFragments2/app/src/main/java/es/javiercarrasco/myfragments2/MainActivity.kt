616ddc74bc68abdb8ff6c394ee98400e6f6f77c4
Javier Carrasco <carrasco.javi@gmail.com>
2023-03-13 12:49:30 +0100

    Capítulo 11
    Fragments

--- CAP-11/MyFragments2/app/src/main/java/es/javiercarrasco/myfragments2/MainActivity.kt
+++ CAP-11/MyFragments2/app/src/main/java/es/javiercarrasco/myfragments2/MainActivity.kt
@@ -1,8 +1,7 @@
 package es.javiercarrasco.myfragments2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import es.javiercarrasco.myfragments2.databinding.ActivityMainBinding
 
 internal const val ARG_NUMFRAG = "numFrag"
@@ -10,12 +9,44 @@ internal const val ARG_COLORBACK = "colorBack"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var numfrag = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Se muestra el primer fragment.
+        showFragment()
+        binding.button.setOnClickListener {
+            showFragment()
+        }
+    }
 
+    private fun showFragment() {
+        val transaction = supportFragmentManager.beginTransaction()
+
+        // Declaración del Fragment mediante newInstance.
+        val fragment = NewFragment.newInstance(
+            ++numfrag,
+            (if ((numfrag % 2) == 0) getColor(R.color.colorPar) else getColor(R.color.colorImpar))
+        )
+
+        // Declaración básica del Fragment.
+//        val fragment = NewFragment()
+        // Se crea la variable Bundle para "empaquetar" los datos a pasar.
+//        val bundle = Bundle()
+//        bundle.putInt(ARG_NUMFRAG, ++numfrag)
+//        bundle.putInt(
+//            ARG_COLORBACK,
+//            (if ((numfrag % 2) == 0)
+//                getColor(R.color.colorPar)
+//            else getColor(R.color.colorImpar))
+//        )
+//        fragment.arguments = bundle
+
+        transaction.replace(binding.fragmentHolder.id, fragment)
+        transaction.disallowAddToBackStack()
+        transaction.commit()
     }
 }
