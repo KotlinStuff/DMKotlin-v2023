bab11c18e27e065bfbee0850e466a4b227f0af72
Javier Carrasco <carrasco.javi@gmail.com>
2023-03-14 17:00:06 +0100

    Capítulo 11
    Fragments

---
+++ CAP-11/MyFragments3/app/src/main/java/es/javiercarrasco/myfragments3/MainActivity.kt
@@ -0,0 +1,39 @@
+package es.javiercarrasco.myfragments3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import es.javiercarrasco.myfragments3.databinding.ActivityMainBinding
+
+internal const val ARG_NUMFRAG = "numFrag"
+internal const val ARG_COLORBACK = "colorBack"
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var numfrag = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Se muestra el primer fragment.
+        showFragment()
+        binding.button.setOnClickListener {
+            showFragment()
+        }
+    }
+
+    private fun showFragment() {
+        val transaction = supportFragmentManager.beginTransaction()
+
+        // Declaración del Fragment mediante newInstance.
+        val fragment = NewFragment.newInstance(
+            ++numfrag,
+            (if ((numfrag % 2) == 0) getColor(R.color.colorPar) else getColor(R.color.colorImpar))
+        )
+
+        transaction.replace(binding.fragmentHolder.id, fragment)
+        transaction.disallowAddToBackStack()
+        transaction.commit()
+    }
+}
