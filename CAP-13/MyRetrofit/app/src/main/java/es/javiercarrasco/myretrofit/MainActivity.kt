d445ecc91d989d7120a36b9077ff143d9e7cb4a6
Javier Carrasco <carrasco.javi@gmail.com>
2023-07-10 14:00:29 +0200

    Capítulo 13
    HTTPS y servicios web

--- CAP-13/MyRetrofit/app/src/main/java/es/javiercarrasco/myretrofit/MainActivity.kt
+++ CAP-13/MyRetrofit/app/src/main/java/es/javiercarrasco/myretrofit/MainActivity.kt
@@ -6,6 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.GridLayoutManager
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import es.javiercarrasco.myretrofit.adapters.ProductsRecyclerAdapter
 import es.javiercarrasco.myretrofit.data.StoreDataSource
 import es.javiercarrasco.myretrofit.data.StoreRepository
 import es.javiercarrasco.myretrofit.databinding.ActivityMainBinding
@@ -24,31 +27,46 @@ class MainActivity : AppCompatActivity() {
         MainViewModelFactory(storeRepository)
     }
 
+    private val adapter: ProductsRecyclerAdapter by lazy {
+        ProductsRecyclerAdapter(onClickProduct = { product ->
+            MaterialAlertDialogBuilder(this).apply {
+                setTitle(product.title)
+                setMessage(product.description)
+                setPositiveButton(getString(android.R.string.ok)) { dialog, _ ->
+                    dialog.dismiss()
+                }
+            }.show()
+        })
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.textView.text = getString(R.string.txtCargando)
+        // binding.textView.text = getString(R.string.txtCargando)
+        binding.recyclerProducts.layoutManager = GridLayoutManager(this, 2)
+        binding.recyclerProducts.adapter = adapter
 
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 vm._products
                     .catch {
-                        binding.textView.text = "ERROR: ${it.toString()}"
+                        println("ERROR: ${it.toString()}")
                     }
                     .collect {
-                        binding.textView.text = ""
-                        it.forEach {
-                            binding.textView.append(
-                                "${it.id}\n" +
-                                        "${it.title}\n" +
-                                        "${it.category}\n" +
-                                        "${it.description}\n" +
-                                        "${it.price}\n" +
-                                        "${it.image}\n\n"
-                            )
-                        }
+                        adapter.submitList(it)
+//                        binding.textView.text = ""
+//                        it.forEach {
+//                            binding.textView.append(
+//                                "${it.id}\n" +
+//                                        "${it.title}\n" +
+//                                        "${it.category}\n" +
+//                                        "${it.description}\n" +
+//                                        "${it.price}\n" +
+//                                        "${it.image}\n\n"
+//                            )
+//                        }
                     }
             }
         }
