2469656f963c4cdc2888b69a6d4427f23487b65e
Javier Carrasco <carrasco.javi@gmail.com>
2023-08-02 19:53:31 +0200

    Capítulo 13
    HTTPS y servicios web

--- CAP-13/MyRetrofit_Flow/app/src/main/java/es/javiercarrasco/myretrofit/data/Retrofit2Api.kt
+++ CAP-13/MyRetrofit_Flow/app/src/main/java/es/javiercarrasco/myretrofit/data/Retrofit2Api.kt
@@ -3,6 +3,7 @@ package es.javiercarrasco.myretrofit.data
 import es.javiercarrasco.myretrofit.domain.model.Login
 import es.javiercarrasco.myretrofit.domain.model.Products
 import kotlinx.coroutines.flow.Flow
+import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Field
@@ -42,11 +43,3 @@ interface FakestoreApi {
     @POST("auth/login")
     suspend fun login(@Field("username") user: String, @Field("password") pass: String): Login
 }
-
-//interface FakestoreApiFlow {
-//    fun getProducts(): Flow<List<Products>>
-//    fun getCategories(): Flow<List<String>>
-//    fun getProductsByCategory(category: String): Flow<List<Products>>
-//    suspend fun getProductById(id: Int): Products?
-//    suspend fun login(user: String, pass: String): Login
-//}
