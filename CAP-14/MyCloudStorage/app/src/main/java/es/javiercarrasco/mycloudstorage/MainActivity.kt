5e397a5579853ffa231ccf1ed569d31111ab8ced
Javier Carrasco <carrasco.javi@gmail.com>
2023-10-16 12:57:35 +0200

    Capítulo 14
    Firebase

---
+++ CAP-14/MyCloudStorage/app/src/main/java/es/javiercarrasco/mycloudstorage/MainActivity.kt
@@ -0,0 +1,17 @@
+package es.javiercarrasco.mycloudstorage
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import es.javiercarrasco.mycloudstorage.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+}
