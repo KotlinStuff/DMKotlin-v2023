152edd1c0eafadce953447c394782e045ed4a299
Javier Carrasco <carrasco.javi@gmail.com>
2023-11-29 17:37:04 +0100

    Capítulo 15

--- CAP-15/MyFirstMapbox/settings.gradle.kts
+++ CAP-15/MyFirstMapbox/settings.gradle.kts
@@ -10,6 +10,20 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+
+        maven {
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+            url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
+            credentials {
+                // Do not change the username below.
+                // This should always be `mapbox` (not your username).
+                username = "mapbox"
+                // Use the secret token you stored in gradle.properties as the password
+                password = System.getProperty("MAPBOX_DOWNLOADS_TOKEN") ?: ""
+            }
+        }
     }
 }
 
