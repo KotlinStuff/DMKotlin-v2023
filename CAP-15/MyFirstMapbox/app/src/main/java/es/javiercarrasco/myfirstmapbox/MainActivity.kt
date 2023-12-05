bb5cdb0409f4e6f30a1972ae867f3a95d8301fff
Javier Carrasco <carrasco.javi@gmail.com>
2023-12-05 17:54:20 +0100

    Capítulo 15

--- CAP-15/MyFirstMapbox/app/src/main/java/es/javiercarrasco/myfirstmapbox/MainActivity.kt
+++ CAP-15/MyFirstMapbox/app/src/main/java/es/javiercarrasco/myfirstmapbox/MainActivity.kt
@@ -1,9 +1,18 @@
 package es.javiercarrasco.myfirstmapbox
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.mapbox.geojson.Point
 import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.Style
+import com.mapbox.maps.extension.style.layers.generated.FillLayer
+import com.mapbox.maps.extension.style.layers.getLayer
+import com.mapbox.maps.extension.style.layers.getLayerAs
+import com.mapbox.maps.plugin.attribution.attribution
+import com.mapbox.maps.plugin.compass.compass
+import com.mapbox.maps.plugin.logo.logo
 import es.javiercarrasco.myfirstmapbox.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -23,5 +32,19 @@ class MainActivity : AppCompatActivity() {
                 .bearing(0.0) // rotación, 0.0 es norte
                 .build()
         )
+
+        // Carga de un estilo desde al API Estilos de Mapbox.
+        mapbox.loadStyle(Style.OUTDOORS) { style ->
+            // Agua de color rojo.
+            val singleLayer = style.getLayerAs<FillLayer>("water")
+            singleLayer!!.fillColor(Color.RED)
+        }
+        //mapbox.loadStyle("mapbox://styles/mapbox/dark-v11")
+
+        // Ya se puede añadir información o hacer otros ajustes.
+        binding.mapView.logo.enabled = true // Desactiva el logo.
+        binding.mapView.attribution.enabled = false // Desactiva la atribución.
+        // Mantiene la brújula siempre activada.
+        binding.mapView.compass.fadeWhenFacingNorth = false
     }
 }
