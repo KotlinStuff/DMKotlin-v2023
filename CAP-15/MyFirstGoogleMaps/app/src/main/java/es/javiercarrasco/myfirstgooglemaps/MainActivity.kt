6fe691160837dd34d3fadfcf29a0eea8afb6487a
Javier Carrasco <carrasco.javi@gmail.com>
2023-12-20 17:07:50 +0100

    Capítulo 15

--- CAP-15/MyFirstGoogleMaps/app/src/main/java/es/javiercarrasco/myfirstgooglemaps/MainActivity.kt
+++ CAP-15/MyFirstGoogleMaps/app/src/main/java/es/javiercarrasco/myfirstgooglemaps/MainActivity.kt
@@ -1,11 +1,25 @@
 package es.javiercarrasco.myfirstgooglemaps
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import es.javiercarrasco.myfirstgooglemaps.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+
     }
 }
